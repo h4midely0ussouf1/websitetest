59c159f5f07cb4866248507c8fcb51ce93a3bee7
h4mid ely0ussouf1 <elyoussoufihamid818@gmail.com>
2026-02-21 14:57:47 +0000

    v

--- program.py
+++ program.py
@@ -1,3 +1,12 @@
 import flask
 import bcrypt
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "test"
+
+if __name__ == "__main__":
+    app.run(debug=True)
