5365ffe0bd7ecb43cea2c176e58e15abf3bd8aab
h4mid ely0ussouf1 <elyoussoufihamid818@gmail.com>
2026-02-21 14:59:40 +0000

    c

--- program.py
+++ program.py
@@ -1,12 +1,12 @@
 import flask
 import bcrypt
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "test"
+    return "index.html"
 
 if __name__ == "__main__":
     app.run(debug=True)
